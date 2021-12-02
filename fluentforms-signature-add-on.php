9fcc3d8c3381053f778024a53f24c673a32bbf3f
abusohib <abushoaib73@gmail.com>
2021-12-02 21:58:52 +0600

    Plugin file init

---
+++ fluentforms-signature-add-on.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @package   	      Fluent Forms Signature Contract Add-on
+ * @contributors      Kevin Michael Gray (Approve Me), Abu Sohib (Approve Me)
+ * @wordpress-plugin
+ * Plugin Name:       Fluent Forms Signature Contract Add-on by ApproveMe
+ * Plugin URI:        http://aprv.me/2lfrDYG
+ * Description:       This add-on makes it possible to automatically email a WP E-Signature document (or redirect a user to a document) after the user has succesfully submitted a Fluent Forms. You can also insert data from the submitted Fluent Form into the WP E-Signature document.
+ * Version:           1.0.0
+ * Author:            Approve Me
+ * Author URI:        https://www.approveme.com/
+ * Text Domain:       esig-ff
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if (!defined('WPINC')) {
+    die;
+}
