de092f0ef7dce23794b1c26e2e17283b4dd95893
abusohib <abushoaib73@gmail.com>
2022-05-25 18:11:23 +0600

    small changes

--- admin/assets/js/esig-fluentform-setting-control.js
+++ admin/assets/js/esig-fluentform-setting-control.js
@@ -2,9 +2,6 @@ var url = String(window.location);
 var hasEsig = url.substr(url.length - 12);
 
 if(hasEsig == 'wpesignature'){
+   // remove General SmartCodes js file goes here
 
-    document.getElementsByClassName('el-dropdown-menu')[0].setAttribute('style','display:none !important');
-
-
-    
 }
